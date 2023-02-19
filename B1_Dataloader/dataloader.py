fe678ee8cdae513f4135f2674b723c73a73a3769
pdymek <pd37300@gmail.com>
2023-02-19 16:50:49 +0100

    update v2

--- B1_Dataloader/dataloader.py
+++ B1_Dataloader/dataloader.py
@@ -14,3 +14,6 @@ class DataLoader_(DataLoader):
     # print("Dataloader executed!")
     def __init__(self, **kwargs):
         super(DataLoader_, self).__init__(**kwargs)
+        
+        
+## test        
