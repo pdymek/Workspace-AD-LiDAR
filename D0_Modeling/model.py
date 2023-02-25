993d86586dd8e8e6befecb06905374d13211d8b5
NilOller <60654494+NilOller@users.noreply.github.com>
2023-02-25 20:16:18 +0100

    Update model.py
    Update of the model.py wih some features modified.

--- D0_Modeling/model.py
+++ D0_Modeling/model.py
@@ -76,21 +76,20 @@ class BasePointNet(nn.Module):
     def forward(self, x):
         num_points = x.shape[1]  # torch.Size([BATCH, SAMPLES, DIMS])
 
-        x_tnet = x[:, :, :2]  # only apply T-NET to x and y
+        x_tnet = x[:, :, :3]  # only apply T-NET to x and y andz
         input_transform = self.input_transform(x_tnet)
         x_tnet = torch.bmm(x_tnet, input_transform)  # Performs a batch matrix-matrix product
-        x_tnet = torch.cat([x_tnet, x[:, :, 2].unsqueeze(2), x[:, :, 4].unsqueeze(2)], dim=2)  # concat z and intensity
-        x_tnet = torch.cat([x_tnet, x[:, :, 6].unsqueeze(2), x[:, :, 7].unsqueeze(2), x[:, :, 9].unsqueeze(2)],dim=2)  # concat Green Blue NDVI
+        x_tnet = torch.cat([x_tnet, x[:, :, 3].unsqueeze(2), x[:, :, 4].unsqueeze(2)], dim=2)  # concat z and reflection
         x_tnet = x_tnet.transpose(2, 1)  # [batch, dims, n_points]
 
         x = F.relu(self.bn_1(self.conv_1(x_tnet)))
-        x = F.relu(self.bn_2(self.conv_2(x)))  # [batch, 64, 2000]
-        x = x.transpose(2, 1)  # [batch, 2000, 64]
+        x = F.relu(self.bn_2(self.conv_2(x)))  # [batch, 64, 4000]
+        x = x.transpose(2, 1)  # [batch, 4000, 64]
 
         feature_transform = self.feature_transform(x)  # [batch, 64, 64]
 
         x = torch.bmm(x, feature_transform)
-        local_point_features = x  # [batch, 2000, 64]
+        local_point_features = x  # [batch, 4000, 64]
 
         x = x.transpose(2, 1)
         x = F.relu(self.bn_3(self.conv_3(x)))
@@ -104,64 +103,3 @@ class BasePointNet(nn.Module):
             return torch.cat([global_feature.transpose(2, 1), local_point_features], 2), feature_transform
         else:
             return global_feature, feature_transform
-
-
-class ClassificationPointNet(nn.Module):
-
-    def __init__(self, num_classes, dropout=0.3, point_dimension=3, dataset=''):
-        super(ClassificationPointNet, self).__init__()
-        self.dataset = dataset
-
-        self.base_pointnet = BasePointNet(return_local_features=False, point_dimension=point_dimension,
-                                          dataset=self.dataset)
-
-        self.fc_1 = nn.Linear(1024, 512)
-        self.fc_2 = nn.Linear(512, 256)
-        self.fc_3 = nn.Linear(256, num_classes)
-
-        self.bn_1 = nn.BatchNorm1d(512)
-        self.bn_2 = nn.BatchNorm1d(256)
-
-        self.dropout_1 = nn.Dropout(dropout)
-
-    def forward(self, x):
-        global_feature, feature_transform = self.base_pointnet(x)
-
-        x = F.relu(self.bn_1(self.fc_1(global_feature)))
-        x = F.relu(self.bn_2(self.fc_2(x)))
-        x = self.dropout_1(x)
-
-        return F.log_softmax(self.fc_3(x), dim=1), feature_transform
-
-
-class SegmentationPointNet(nn.Module):
-
-    def __init__(self, num_classes, point_dimension=3):
-        super(SegmentationPointNet, self).__init__()
-        self.base_pointnet = BasePointNet(return_local_features=True, point_dimension=point_dimension)
-
-        self.conv_1 = nn.Conv1d(1088, 512, 1)
-        self.conv_2 = nn.Conv1d(512, 256, 1)
-        self.conv_3 = nn.Conv1d(256, 128, 1)
-        self.conv_4 = nn.Conv1d(128, num_classes, 1)
-
-        self.bn_1 = nn.BatchNorm1d(512)
-        self.bn_2 = nn.BatchNorm1d(256)
-        self.bn_3 = nn.BatchNorm1d(128)
-
-    def forward(self, x):
-        local_global_features, feature_transform = self.base_pointnet(x)
-        x = local_global_features
-        x = x.transpose(2, 1)
-        x = F.relu(self.bn_1(self.conv_1(x)))
-        x = F.relu(self.bn_2(self.conv_2(x)))
-        x = F.relu(self.bn_3(self.conv_3(x)))
-
-        x = self.conv_4(x)
-        x = x.transpose(2, 1)
-
-        return F.log_softmax(x, dim=-1), feature_transform
-
-def Modeling():
-    #TODO
-    print("Modeling executed!")
